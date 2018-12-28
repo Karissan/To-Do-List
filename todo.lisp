ade5cb1d73a6231d1f75b0fd1ab26f339bc95eb6
Karissan <moxwing@gmail.com>
2018-12-27 18:54:45 -0800

    Fixed errors with delete function. Still need to make deletes permanent.

--- todo.lisp
+++ todo.lisp
@@ -6,9 +6,10 @@
       ;;Created something like a for loop (dotimes) that runs code with i as the nth time with the upper bound being length data
       (dotimes (i (length data))
         (let ((line (nth i data)))
-          (format t "(~a) ~a~%" i line)))
+          (format t "(~a) ~a~%" (1+ i) line)))
       (format t "~%To-Do List Menu~%")
       (format t "(a)dd~%")
+      (format t "(d)elete~%")
       (format t "(q)uit~%")
       (format t "(s)ave~%")
       (format t "~%Select an option~%")
@@ -28,14 +29,15 @@
            (format t "Enter number: ")
            (let ((index (read)))
              (etypecase index
-               (integer (if (< 0 index (length data))
+               (integer (if (< 0 index (1+ (length data)))
                             (progn
                               (setf data (append (subseq data 0 (1- index))
                                                  (subseq data index (length data))))
                               (return))
                             (format t "Invalid input~%"))
-                (symbol (if (eq index 'c')
-                            (return)))))
+                )
+               (symbol (if (eq index 'c)
+                           (return))))
              )))
          (#\s
           (format t "Add later~%"))
